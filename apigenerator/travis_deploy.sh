b0ed1e595c8971a69976bcf146a73da452cd1732
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2016-12-01 11:33:28 +0100

    Update travis_deploy script

--- apigenerator/travis_deploy.sh
+++ apigenerator/travis_deploy.sh
@@ -4,9 +4,9 @@
 # commit on master branch and to deploy it to the vulk-api repository.
 # API_USER and API_PASS are travis secure variable
 
-git clone https://$(API_USER):$(API_PASS)@github.com/realitix/vulk-api.git vulk-api > /dev/null 2>&1
+git clone https://$API_USER:$API_PASS@github.com/realitix/vulk-api.git vulk-api > /dev/null 2>&1
 rm -rf vulk-api/*
 python setup.py api
 git --git-dir vulk-api -A
-git --git-dir vulk-api commit -m "$(TRAVIS_COMMIT)"
+git --git-dir vulk-api commit -m "$TRAVIS_COMMIT"
 git --git-dir vulk-api push origin master > /dev/null 2>&1
