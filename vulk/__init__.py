5ebc53fccd79e479d1a39cf02160c8eb2eab247a
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-01-10 15:11:18 +0100

    Add Path to Vulk package

--- vulk/__init__.py
+++ vulk/__init__.py
@@ -2,4 +2,10 @@
 
 Cross-plateform 3D engine
 """
+from os import path as p
+
 __version__ = "0.2.0"
+
+PATH_VULK = p.dirname(p.abspath(__file__))
+PATH_VULK_ASSET = p.join(PATH_VULK, 'asset')
+PATH_VULK_SHADER = p.join(PATH_VULK_ASSET, 'shader')
