ce18939a9ee952906677b7a6be8a42e06dc21fb0
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-01-10 15:15:06 +0100

    Update Matrices

--- vulk/math/matrix.py
+++ vulk/math/matrix.py
@@ -8,15 +8,23 @@ import numpy as np
 class Matrix():
     '''Base class for Matrix'''
     def __init__(self, values):
-        self._values = np.array(values, dtype=np.float32)
+        self._values = np.matrix(values, dtype=np.float32)
 
     @property
     def values(self):
         return self._values
 
+    def set(self, values, offset=0):
+        self._values[offset:] = values
+
+    def mul(self, matrix):
+        self._values = np.matmul(self._values, matrix.values)
+
 
 class Matrix3(Matrix):
     '''Matrix3 class'''
+    dtype = ('Matrix3', np.float32, (3, 3))
+
     def __init__(self, *args):
         if not args:
             super().__init__([[1, 0, 0],
@@ -30,6 +38,8 @@ class Matrix3(Matrix):
 
 class Matrix4(Matrix):
     '''Matrix4 class'''
+    dtype = ('Matrix4', np.float32, (4, 4))
+
     def __init__(self, *args):
         if not args:
             super().__init__([[1, 0, 0, 0],
