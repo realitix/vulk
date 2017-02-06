4ffa1de192e1306817e14d920174a96dd22a5fdc
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-02-06 08:07:43 +0100

    Update matrix

--- vulk/math/matrix.py
+++ vulk/math/matrix.py
@@ -8,6 +8,8 @@ http://www.codinglabs.net/article_world_view_projection_matrix.aspx
 '''
 import numpy as np
 
+from vulk.math.vector import Vector3
+
 
 class Matrix():
     '''Base class for Matrix'''
@@ -21,11 +23,24 @@ class Matrix():
         '''
         self._values = np.array(values, dtype=np.float32)
 
+    def __len__(self):
+        '''Return the matrix size'''
+        return len(self._values)
+
     @property
     def values(self):
         return self._values
 
-    def set(self, values, offset=0):
+    def set(self, matrix):
+        '''Set this matrix to `matrix`
+
+        *Parameters:*
+
+        - `matrix`: `Matrix` to set
+        '''
+        return self.set2(matrix.values)
+
+    def set2(self, values, offset=0):
         '''Set this matrix to `values` from `offset`
 
         *Parameters:*
@@ -34,15 +49,7 @@ class Matrix():
         - `offset`: Start of the update
         '''
         self._values[offset:] = values
-
-    def to_matrix(self, matrix):
-        '''Set this matrix's values to the `matrix` values.
-
-        *Parameters:*
-
-        - `matrix`: Other matrix of the same size
-        '''
-        self.set(matrix.values)
+        return self
 
 
 class Matrix3(Matrix):
@@ -86,6 +93,18 @@ class Matrix4(Matrix):
         # Tmp property used during computation
         self.tmp = np.zeros(16, dtype=np.float32)
 
+    def __str__(self):
+        '''Return a beautiful readable matrix'''
+        v = self._values
+        return ('{} {} {} {}\n'
+                '{} {} {} {}\n'
+                '{} {} {} {}\n'
+                '{} {} {} {}\n'.format(
+                   v[0], v[4], v[8], v[12],
+                   v[1], v[5], v[9], v[13],
+                   v[2], v[6], v[10], v[14],
+                   v[3], v[7], v[11], v[15]))
+
     def mul(self, matrix):
         '''Multiply this matrix by `matrix`
         The order of operation is: `this @ matrix`.
@@ -101,16 +120,84 @@ class Matrix4(Matrix):
         view2.shape = (4, 4)
         self.tmp.shape = (4, 4)
 
-        np.matmul(view1, view2, out=self.tmp)
+        np.matmul(view2, view1, out=self.tmp)
 
         self.tmp.shape = (16,)
         self._values[:] = self.tmp
 
+        return self
+
     def to_identity(self):
         '''Set this matrix to identity matrix'''
-        self.values[:] = 0
-        self.values[::4] = 1
+        self._values[:] = 0.
+        self._values[0] = 1.
+        self._values[5] = 1.
+        self._values[10] = 1.
+        self._values[15] = 1.
 
+        return self
+
+
+class ViewMatrix(Matrix4):
+    '''This class represents a view Matrix.
+
+    View Matrix convert vertex from World space to View space.
+    '''
+    def to_look_at_direction(self, direction, up):
+        '''
+        Set this matrix to a *look at* matrix with a `direction` and a `up`
+        vector.
+
+        *Parameters:*
+
+        - `direction`: Direction `Vector3`
+        - `up`: Up `Vector3`
+        '''
+        vec_z = Vector3(direction).nor()
+        vec_x = Vector3(direction).nor()
+        vec_x.crs(up.nor()).nor()
+        vec_y = Vector3(vec_x).crs(vec_z).nor()
+
+        self.to_identity()
+        self.values[0] = vec_x.x
+        self.values[4] = vec_x.y
+        self.values[8] = vec_x.z
+        self.values[1] = vec_y.x
+        self.values[5] = vec_y.y
+        self.values[9] = vec_y.z
+        self.values[2] = -vec_z.x
+        self.values[6] = -vec_z.y
+        self.values[10] = -vec_z.z
+
+        return self
+
+    def to_look_at(self, position, target, up):
+        '''
+        Set this matrix to a *look at* matrix with a `position`, `target` and
+        `up` vector.
+
+        *Parameters:*
+
+        - `position`: Position `Vector3`
+        - `direction`: Direction `Vector3`
+        - `up`: Up `Vector3`
+        '''
+        # http://www.cs.virginia.edu/~gfx/Courses/1999/intro.fall99.html/lookat.html
+        vec = Vector3(target).sub(position)
+        self.to_look_at_direction(vec, up)
+        translation = TransformationMatrix().to_translation(
+            -position.x, -position.y, -position.z)
+        self.mul(translation)
+
+        return self
+
+
+class ProjectionMatrix(Matrix4):
+    '''This class represents a projection Matrix.
+
+    Projection is the last step to compute Vertex position.
+    It must be applied after the view matrix.
+    '''
     def to_orthographic_2d(self, x, y, width, height, near=0, far=1):
         '''Set this matrix to an orthographic matrix used in 2D rendering
 
@@ -123,7 +210,7 @@ class Matrix4(Matrix):
         - `near`: Near plane
         - `Far`: Far plane
         '''
-        self.to_orthographic(x, x + width, y, y + height, near, far)
+        return self.to_orthographic(x, x + width, y, y + height, near, far)
 
     def to_orthographic(self, left, right, bottom, top, near, far):
         '''
@@ -141,19 +228,58 @@ class Matrix4(Matrix):
         **Note: Vulkan coordinate system is not the same as OpenGL,
                 thus the matrix is adapted to Vulkan**
         '''
+        # https://matthewwellings.com/blog/the-new-vulkan-coordinate-system/
+
+        # OpenGL Orthographic matrix
+        # x_orth = 2 / (right - left)
+        # y_orth = 2 / (top - bottom)
+        # z_orth = -2 / (far - near)
+
+        # tx = -(right + left) / (right - left)
+        # ty = -(top + bottom) / (top - bottom)
+        # tz = -(far + near) / (far - near)
+
+        # Vulkan Orthographic matrix
         x_orth = 2 / (right - left)
-        y_orth = 2 / (top - bottom)
-        z_orth = 1 / (far - near)
+        y_orth = -2 / (top - bottom)
+        z_orth = -1 / (far - near)
 
         tx = -(right + left) / (right - left)
-        ty = -(top + bottom) / (top - bottom)
-        tz = 0
+        ty = (top + bottom) / (top - bottom)
+        tz = -0.5 * (far + near) / (far - near) + 0.5
 
-        self.values[:] = 0
+        self.to_identity()
         self.values[0] = x_orth
         self.values[5] = y_orth
         self.values[10] = z_orth
         self.values[12] = tx
         self.values[13] = ty
         self.values[14] = tz
-        self.values[15] = 1
+
+        return self
+
+
+class TransformationMatrix(Matrix4):
+    '''This class represents a transformation Matrix.
+
+    It's a Matrix4 with added capabilities used to transform model space
+    to world space.
+    '''
+    def to_translation(self, x, y, z):
+        '''
+        Set this matrix to a translation matrix.
+        First set it to identity and then set the 4th column to the
+        translation vector.
+
+        *Parameters:*
+
+        - `x`: x-component of translation vector
+        - `y`: y-component of translation vector
+        - `z`: z-component of translation vector
+        '''
+        self.to_identity()
+        self._values[12] = x
+        self._values[13] = y
+        self._values[14] = z
+
+        return self
