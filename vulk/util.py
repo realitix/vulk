410214239810826d626dc6ca4e62366d7c2b20c4
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-02-13 08:09:02 +0100

    Add nanos to util.py

--- vulk/util.py
+++ vulk/util.py
@@ -6,6 +6,11 @@ def millis():
     return time.perf_counter() * 1000
 
 
+def nanos():
+    '''Return the time in nanoseconds'''
+    return millis() * 1000000
+
+
 def time_since_millis(previous_time):
     '''Return the time in millisecons from the previous_time argument'''
     return millis() - previous_time
