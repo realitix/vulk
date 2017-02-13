0a98ea5b07c75ca5d6cf275175529b5b326fd1d7
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-02-13 08:08:40 +0100

    Update rectangle

--- vulk/math/shape.py
+++ vulk/math/shape.py
@@ -8,3 +8,7 @@ class Rectangle():
     def contains(self, x, y):
         return (self.x <= x and self.x + self.width >= x and
                 self.y <= y and self.y + self.height >= y)
+
+    def overlaps(self, r):
+        return self.x < r.x + r.width and self.x + self.width > r.x and \
+               self.y < r.y + r.height and self.y + self.height > r.y
