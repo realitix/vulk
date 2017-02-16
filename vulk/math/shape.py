5ff8d6ecd18b10cc59e31d15211ce0b936aad3e3
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-02-16 19:10:16 +0100

    Add methods to Rectangle

--- vulk/math/shape.py
+++ vulk/math/shape.py
@@ -1,9 +1,19 @@
 class Rectangle():
-    def __init__(self, x, y, width, height):
+    def __init__(self, x=0, y=0, width=0, height=0):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+
+    def set(self, rectangle):
+        self.x = rectangle.x
+        self.y = rectangle.y
+        self.width = rectangle.width
+        self.height = rectangle.height
+
+    def __repr__(self):
+        return 'Rectangle[x={}, y={}, width={}, height={}]'.format(
+            self.x, self.y, self.width, self.height)
 
     def contains(self, x, y):
         return (self.x <= x and self.x + self.width >= x and
