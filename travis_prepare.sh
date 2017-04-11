b6a4555bb0fd364d56f2c81a6b085fb9da411ce3
Jean-Sebastien Bevilacqua <realitix@gmail.com>
2017-04-11 10:08:58 +0200

    Update sdk link

--- travis_prepare.sh
+++ travis_prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Download Vulkan SDK
-wget https://vulkan.lunarg.com/sdk/download/1.0.46.0/linux/vulkansdk-linux-x86_64-1.0.46.0.run
+wget http://jsbevilacqua.com/vulkansdk-linux-x86_64-1.0.46.0.run
 chmod u+x vulkansdk-linux-x86_64-1.0.46.0.run
 ./vulkansdk-linux-x86_64-1.0.46.0.run
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
